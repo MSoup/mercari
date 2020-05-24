8e7c24f05321890ea55728bb271da08207b1e57c
Daven <davenlu1989@gmail.com>
2020-05-24 17:03:26 +0900

    changing output to json

--- mercari.py
+++ mercari.py
@@ -1,34 +1,43 @@
 #Mercari App - v0.01
 import requests
 from lxml import html
-
+import csv
+import os
+import json
 
 #Set user-agent
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
 headers = {'User-Agent': user_agent}
+my_listings_page = os.environ['LISTING_URL']
 
-#INSERT MERCARI LISTINGS PAGE URL
-with open('listings_url.txt', 'r') as url:
-    listings_page = url.read()
+response = requests.get(my_listings_page, headers = headers)
 
-response = requests.get(listings_page, headers = headers)
-
-assert response.status_code == 200, 'Did not proper access page, time to debug!'
+if not response.status_code == 200:
+    print('Did not proper access page, time to debug!')
 
 tree = html.fromstring(response.content)
 
 #Collect all listings into a list
 listings_list = tree.xpath('/html/body/div/section/section/div/section')
 
-#Generate listing:price pairs
-items = {}
+listings = []
 
-#TODO - Include sold tag
-for i in range(1,len(listings_list) + 1):
-    items[tree.xpath(f'/html/body/div/section/section/div/section[{i}]/a/div/h3/text()')[0]] = tree.xpath(f'/html/body/div/section/section/div/section[{i}]/a/div/div/div/text()')[0]
+for index in range(1,len(listings_list) + 1):
 
-print("This is your entire listing history (includes sold items)")
+    listing = {}
+    listing["item"] = tree.xpath(f'/html/body/div/section/section/div/section[{index}]/a/div/h3/text()')[0]
+    listing["item_price"] = tree.xpath(f'/html/body/div/section/section/div/section[{index}]/a/div/div/div/text()')[0]
+    listing["listing_url"] = tree.xpath(f'/html/body/div/section/section/div/section[{index}]/a/@href')[0]
+    listing["sold"] = len(tree.xpath(f"/html/body/div/section/section/div/section[{index}]/a/figure/figcaption//div[contains(@class,'item-sold-out-badge')]")) > 0
+    listings.append(listing)
 
-#TODO - How can I format this nicely?
-for index, item in enumerate(items):
-    print(f'{index + 1} - {item}{" "*(50-len(item))}: listed for {items[item]}')
+x = json.dumps(listings, sort_keys=True, indent=4, ensure_ascii=False)
+
+with open("test", "w", encoding="utf8") as f:
+    f.write(x)
+
+
+"""
+y = json.loads(x)
+"""
+
